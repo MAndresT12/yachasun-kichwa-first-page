6ca5a2481cb1a18199ecc06aa78eb11611856ba9
Santiago León <sleoncamacho@outlook.com>
2024-09-15 15:57:32 -0500

    Update FirstNumbers.jsx

--- src/components/screens/basic/module1/FirstNumbers.jsx
+++ src/components/screens/basic/module1/FirstNumbers.jsx
@@ -115,7 +115,7 @@ const FirstNumbers = () => {
                     </TouchableOpacity>
                 </View>
                 <View style={styles.body}>
-                    <CardDefault title="Los Primeros Números en Kichwa">
+                    <CardDefault title="Una aventura matemática">
                         <Text style={styles.cardContent}>
                             Aquí aprenderemos los primeros números de Español a Kichwa.{"\n\n"}¡Prepárate para tener un tour matemático!
                         </Text>
