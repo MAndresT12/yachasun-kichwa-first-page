529b8a08d46ca237de5f17f755f7fa343123dd35
Santiago León <sleoncamacho@outlook.com>
2025-01-17 19:24:18 -0500

    Update Foods.jsx

--- src/components/screens/basic/module4/Foods.jsx
+++ src/components/screens/basic/module4/Foods.jsx
@@ -30,31 +30,31 @@ const images = {
 };
 
 const foods1_data = [
-    { imageCard: images.foods1, kichwa: "Aycha", spanish: "Carne" },
-    { imageCard: images.foods1, kichwa: "Kachi", spanish: "Sal" },
-    { imageCard: images.foods1, kichwa: "Haku", spanish: "Harina" },
-    { imageCard: images.foods1, kichwa: "Purutu", spanish: "Fréjol" },
-    { imageCard: images.foods1, kichwa: "Makinchu", spanish: "Queso" },
-    { imageCard: images.foods1, kichwa: "Mishki Haku", spanish: "Azúcar" },
-    { imageCard: images.foods1, kichwa: "Tanta", spanish: "Pan" },
-    { imageCard: images.foods1, kichwa: "Yaku Wira", spanish: "Aceite" },
-    { imageCard: images.foods1, kichwa: "Yaku", spanish: "Agua" },
-    { imageCard: images.foods1, kichwa: "Ñuñu", spanish: "Leche" },
-    { imageCard: images.foods1, kichwa: "Chuchi Aycha", spanish: "Pollo" },
+    { imageCard: "https://st5.depositphotos.com/3369547/68921/v/600/depositphotos_689213938-stock-illustration-meat-product-diet-illustration-isolated.jpg", kichwa: "Aycha", spanish: "Carne" },
+    { imageCard: "https://st3.depositphotos.com/11802848/16847/v/600/depositphotos_168476956-stock-illustration-salt-shaker-vector.jpg", kichwa: "Kachi", spanish: "Sal" },
+    { imageCard: "https://st5.depositphotos.com/82542826/73057/v/600/depositphotos_730572268-stock-illustration-global-rice-crisis-high-demand.jpg", kichwa: "Haku", spanish: "Harina" },
+    { imageCard: "https://static9.depositphotos.com/1669000/1199/v/600/depositphotos_11998763-stock-illustration-funny-mexican-cartoon-bean.jpg", kichwa: "Purutu", spanish: "Fréjol" },
+    { imageCard: "https://st5.depositphotos.com/76512116/68766/v/600/depositphotos_687661716-stock-illustration-vector-illustration-piece-cheese-cartoon.jpg", kichwa: "Makinchu", spanish: "Queso" },
+    { imageCard: "https://st.depositphotos.com/1041273/4444/v/600/depositphotos_44446059-stock-illustration-sugar-bag.jpg", kichwa: "Mishki Haku", spanish: "Azúcar" },
+    { imageCard: "https://st2.depositphotos.com/3864435/8164/v/600/depositphotos_81646670-stock-illustration-bread-and-ears-of-wheat.jpg", kichwa: "Tanta", spanish: "Pan" },
+    { imageCard: "https://st.depositphotos.com/2208320/2143/v/600/depositphotos_21439537-stock-illustration-vegetable-oil-plastic-bottle-isolated.jpg", kichwa: "Yaku Wira", spanish: "Aceite" },
+    { imageCard: "https://st2.depositphotos.com/1007168/6106/v/600/depositphotos_61064019-stock-illustration-water-drop-holding-water-glass.jpg", kichwa: "Yaku", spanish: "Agua" },
+    { imageCard: "https://st2.depositphotos.com/1742172/10198/v/600/depositphotos_101983250-stock-illustration-cartoon-milk-carton.jpg", kichwa: "Ñuñu", spanish: "Leche" },
+    { imageCard: "https://st.depositphotos.com/1732591/1280/v/600/depositphotos_12800779-stock-illustration-fried-chicken.jpg", kichwa: "Chuchi Aycha", spanish: "Pollo" },
 ];
 
 const foods2_data = [
-    { imageCard: images.foods1, kichwa: "Challwa", spanish: "Pescado" },
-    { imageCard: images.foods1, kichwa: "Papa", spanish: "Papa" },
-    { imageCard: images.foods1, kichwa: "Palta", spanish: "Aguacate" },
-    { imageCard: images.foods1, kichwa: "Sara", spanish: "Maíz" },
-    { imageCard: images.foods1, kichwa: "Inchik", spanish: "Maní" },
-    { imageCard: images.foods1, kichwa: "Chukllu", spanish: "Choclo" },
-    { imageCard: images.foods1, kichwa: "Kinuwa", spanish: "Quinua" },
-    { imageCard: images.foods1, kichwa: "Lumu", spanish: "Yuca" },
-    { imageCard: images.foods1, kichwa: "Tawri", spanish: "Chocho" },
-    { imageCard: images.foods1, kichwa: "Wiru", spanish: "Caña" },
-    { imageCard: images.foods1, kichwa: "Uchu", spanish: "Ají" },
+    { imageCard: "https://st5.depositphotos.com/60679122/64578/v/600/depositphotos_645782138-stock-illustration-blue-aquarium-fish-icon-editable.jpg", kichwa: "Challwa", spanish: "Pescado" },
+    { imageCard: "https://st3.depositphotos.com/29384342/50556/v/600/depositphotos_505565436-stock-illustration-cartoon-illustration-cute-smiling-potato.jpg", kichwa: "Papa", spanish: "Papa" },
+    { imageCard: "https://st5.depositphotos.com/23146722/66685/v/600/depositphotos_666859454-stock-illustration-nice-flat-vector-illustration-green.jpg", kichwa: "Palta", spanish: "Aguacate" },
+    { imageCard: "https://st5.depositphotos.com/3977543/71875/v/600/depositphotos_718758174-stock-illustration-vibrant-vector-illustration-golden-corn.jpg", kichwa: "Sara", spanish: "Maíz" },
+    { imageCard: "https://st5.depositphotos.com/76834826/70346/v/600/depositphotos_703462726-stock-illustration-cartoon-vector-illustration-peanut.jpg", kichwa: "Inchik", spanish: "Maní" },
+    { imageCard: "https://st5.depositphotos.com/1020070/70731/v/600/depositphotos_707318248-stock-illustration-realistic-sweet-boiled-corn-kernels.jpg", kichwa: "Chukllu", spanish: "Choclo" },
+    { imageCard: "https://st3.depositphotos.com/1028367/19576/v/600/depositphotos_195760854-stock-illustration-quinoa-cereal-deep-bowl-monochrome.jpg", kichwa: "Kinuwa", spanish: "Quinua" },
+    { imageCard: "https://st4.depositphotos.com/5984660/31099/v/600/depositphotos_310994236-stock-illustration-cassava-manioc-plants-with-leaves.jpg", kichwa: "Lumu", spanish: "Yuca" },
+    { imageCard: "https://st2.depositphotos.com/36517744/44443/v/600/depositphotos_444437462-stock-illustration-soybean-cartoon-vector-free-space.jpg", kichwa: "Tawri", spanish: "Chocho" },
+    { imageCard: "https://st4.depositphotos.com/24292998/29005/v/600/depositphotos_290059400-stock-illustration-realistic-bamboo-sticks-with-leaves.jpg", kichwa: "Wiru", spanish: "Caña" },
+    { imageCard: "https://st2.depositphotos.com/4398873/8681/v/600/depositphotos_86816702-stock-illustration-hot-red-pepper.jpg", kichwa: "Uchu", spanish: "Ají" },
 ];
 
 const fruits_data = [
@@ -97,7 +97,7 @@ const renderDataImages = (data) => {
     return data.map((item, index) => (
         <View key={index} style={styles.tableRow}>
             <View style={styles.imageContainer}>
-                <ImageContainer path={item.imageCard} style={styles.animalImage} />
+                <ImageContainer uri={item.imageCard} style={styles.animalImage} />
             </View>
             <Text style={[styles.tableCell, styles.textCenter]}>{item.spanish}</Text>
             <Text style={[styles.tableCell, styles.textCenter]}>{item.kichwa}</Text>
